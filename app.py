f6851fd6d1a87161dae7359e9f2f32407ea47dcf
Luis-Acevedo <arnieacevedo1996@gmail.com>
2021-08-03 21:37:16 -0400

    Completion of 9.4.3
    Successful run of flask app and addition of Stats route

--- app.py
+++ app.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello world'
 
+@app.route('/Stats')
+def stats():
+    return 'Where stats will go'
+# Make sure to run 'export FLASK_APP=app.py' and 'flask run' in terminal
