595c9de5e386f431115ce60a50d2098e473ddec8
Luis-Acevedo <arnieacevedo1996@gmail.com>
2021-08-03 22:15:01 -0400

    Completion of 9.5.3
    Completion of welcome page and routes

--- app.py
+++ app.py
@@ -1,13 +1,46 @@
-from flask import Flask
+# Importing dependencies/libraries
+import datetime as dt
+import numpy as np
+import pandas as pd
 
+# Importing SQLAlchemy dependencies/libraries
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+# Creating the flask app
+from flask import Flask, jsonify
+
+# Establishing database
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# Reflecting database into different bins
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Creating session link from Python to our database
+session = Session(engine)
+
+# Setting up flask app
 app = Flask(__name__)
+
+# Creating Routes
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello world'
-
-@app.route('/Stats')
-def stats():
-    return 'Where stats will go'
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''') 
 # Make sure to run 'export FLASK_APP=app.py' and 'flask run' in terminal
